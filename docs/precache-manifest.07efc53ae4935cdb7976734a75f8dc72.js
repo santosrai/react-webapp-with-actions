18b723a9c44367c17c2dccfd39c6b5e5a3f86fbb
Ubuntu <ubuntu@ip-172-31-41-120.us-east-2.compute.internal>
2019-09-12 05:24:53 +0000

    prod in docs

---
+++ docs/precache-manifest.07efc53ae4935cdb7976734a75f8dc72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f371877df3f2d8d87be2cfbc6857bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c393de0736cad26cf727",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "78b112e0e664d0712c14",
+    "url": "/static/js/2.b5d3beb5.chunk.js"
+  },
+  {
+    "revision": "c393de0736cad26cf727",
+    "url": "/static/js/main.2aa9dfce.chunk.js"
+  },
+  {
+    "revision": "985745ebd212488cde3e",
+    "url": "/static/js/runtime~main.c050e8bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
